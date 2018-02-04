df7e81437dd1561bb121910e27ca219c28c7adef
MicahWW <mwhornstab@gmail.com>
2018-02-04 15:19:14 -0600

    update

--- HW3/hanoi.py
+++ HW3/hanoi.py
@@ -1,29 +1,11 @@
 import time
-#def hanoi(n, source, helper, target):
-#	if n > 0:
-		# move tower of size n - 1 to helper:
-#		hanoi(n - 1, source, target, helper)
-		# move disk from source peg to target peg
-#		if source:
-#			target.append(source.pop())
-		# move tower of size n - 1 from helper to target
-#		hanoi(n - 1, helper, source, target)
-#	print count
-
-#source = [3, 2, 1]
-#target = []
-#helper = []
-#hanoi(len(source), source, helper, target)
-
-#print source, helper, target
-
 count = 0
 def Hanoi(n, src, dst, tmp):
 	if n > 0:
 		global count
 		count += 1
 		Hanoi(n - 1, src, tmp, dst)
-#		print "Move disc", chr(64 + n), "From tower", src, "to tower", dst
+		print "Move disc", n, "From tower", src, "to tower", dst
 		Hanoi(n - 1, tmp, dst, src)
 discs = raw_input("How many discs? ")
 discs = int(discs)
@@ -37,7 +19,7 @@ try:
 	print " "
 	print end - start
 except:
-	print count
+	print "count {}".format(count)
 	end = time.time()
 	print " "
 	print end - start
